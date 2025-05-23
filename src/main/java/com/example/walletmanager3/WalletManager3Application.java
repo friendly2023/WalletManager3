ee079f866a9b431b849c91c9c778584230e6737b
Vladimir <kazimirov.2013@mail.ru>
2025-05-23 19:25:08 +0300

    Add:создан проект

---
+++ src/main/java/com/example/walletmanager3/WalletManager3Application.java
@@ -0,0 +1,13 @@
+package com.example.walletmanager3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletManager3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletManager3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/walletmanager3/WalletManager3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walletmanager3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletManager3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
