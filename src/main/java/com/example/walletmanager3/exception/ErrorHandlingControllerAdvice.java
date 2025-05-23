c12c67848ae2fdadfd21bdadde9b973f48598836
Vladimir <kazimirov.2013@mail.ru>
2025-05-23 21:51:27 +0300

    Add: добавлен get метод для получения кошелька, класс по сборке ошибок ErrorHandlingControllerAdvice

---
+++ src/main/java/com/example/walletmanager3/exception/ErrorHandlingControllerAdvice.java
@@ -0,0 +1,45 @@
+package com.example.walletmanager3.exception;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ErrorHandlingControllerAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse onConstraintValidationException(
+            ConstraintViolationException e
+    ) {
+        final List<Violation> violations = e.getConstraintViolations().stream()
+                .map(
+                        violation -> new Violation(
+                                violation.getPropertyPath().toString(),
+                                violation.getMessage()
+                        )
+                )
+                .collect(Collectors.toList());
+        return new ValidationErrorResponse(violations);
+    }
+
+    @ResponseBody
+    @ExceptionHandler(WalletNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ValidationErrorResponse handleWalletNotFound(
+            WalletNotFoundException e
+    ) {
+        final List<Violation> violations = Collections.singletonList(
+                new Violation("walletId", e.getMessage())
+        );
+        return new ValidationErrorResponse(violations);
+    }
+}
