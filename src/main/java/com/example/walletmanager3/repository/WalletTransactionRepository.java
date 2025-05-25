6cfb3dcab000cdfe4f4037109835dda8212c0202
Vladimir <kazimirov.2013@mail.ru>
2025-05-25 19:43:16 +0300

    Add: проверка на уникальность для walletId кошелька

--- src/main/java/com/example/walletmanager3/repository/WalletTransactionRepository.java
+++ src/main/java/com/example/walletmanager3/repository/WalletTransactionRepository.java
@@ -4,6 +4,9 @@ import com.example.walletmanager3.entity.WalletTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 public interface WalletTransactionRepository extends JpaRepository<WalletTransaction, Integer> {
+    boolean existsByWalletId(UUID walletId);
 }
