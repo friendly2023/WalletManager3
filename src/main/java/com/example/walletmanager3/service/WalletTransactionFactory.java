4b4dc9de368295238853f598b62746c750736f26
vladimir <kazimirov.2013@mail.ru>
2025-06-06 01:03:54 +0300

    Fix: оптимизировал WalletTransactionFactor, удалив лишний метод

--- src/main/java/com/example/walletmanager3/service/WalletTransactionFactory.java
+++ src/main/java/com/example/walletmanager3/service/WalletTransactionFactory.java
@@ -23,26 +23,12 @@ public class WalletTransactionFactory {
                                         BigDecimal amount,
                                         BigDecimal balanceAfter) {
 
-        WalletTransaction walletTransaction = buildWalletTransaction(
-                walletId,
-                operationType,
-                amount,
-                balanceAfter);
-
-        walletTransactionRepository.save(walletTransaction);
-    }
-
-    private WalletTransaction buildWalletTransaction(UUID walletId,
-                                                     OperationType operationType,
-                                                     BigDecimal amount,
-                                                     BigDecimal balanceAfter) {
-
         WalletTransaction walletTransaction = new WalletTransaction();
         walletTransaction.setWalletId(walletId);
         walletTransaction.setOperationType(operationType);
         walletTransaction.setAmount(amount);
         walletTransaction.setBalanceAfter(balanceAfter);
 
-        return walletTransaction;
+        walletTransactionRepository.save(walletTransaction);
     }
 }
